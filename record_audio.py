7b7c0c72d233d70a96836fa65e1223a627436b82
akinoshi <aiju.kinoshita@ontariotechu.net>
2025-04-15 14:00:50 -0400

    updated preprocessing

--- record_audio.py
+++ record_audio.py
@@ -5,8 +5,9 @@ Parameters: output file path, duration in second, sample rate
 
 import sounddevice as sd
 import soundfile as sf
+import numpy as np
 
-sd.default.device = [3,6]
+# sd.default.device = [3,6]
 print('input device used:', sd.query_devices(sd.default.device[0]))
 
 # Record audio
@@ -14,6 +15,16 @@ def record_audio(output_path, duration=5, sample_rate=16000):
     print("Listening...")
     audio = sd.rec(int(duration*sample_rate), samplerate=sample_rate, channels=1, dtype="float32")
     sd.wait() # wait until recording finishes
+    
+    # Preprocess the audio (normalize and handle channels)
+    # TODO(Noise cancelling)
+    # audio = noise_cancelling(audio)
+    audio = audio / np.max(np.abs(audio))  # Normalize
+    if audio.ndim > 1:  # If stereo, take the first channel
+        audio = audio[:, 0]
+    # Convert audio to tensor Float (32-bit float)
+    audio = audio.astype(np.float32)
+    
     sf.write(output_path, audio, sample_rate)
     print(f"Recording saved to {output_path}")
 
@@ -28,6 +39,16 @@ def is_audio_silent(file_path, silence_threshold=-40):
     except Exception as e:
         print("Error", str(e))
         return True
+    
+def noise_cancelling(audio):
+    """
+    Noise cancelling
+    """
+    try:
+        # print("TODO")
+        return audio
+    except Exception as e:
+        print("Error", str(e))
 
 # Example usage
 # record_audio("recorded_audio.wav", duration=5)
